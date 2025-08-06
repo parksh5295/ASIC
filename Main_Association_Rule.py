6f374a08e2366b5da4b30303c843fbf505c6d834
parksh5295 <parksh5295@gmail.com>
2025-08-06 14:27:13 +0900

    cicids

--- Main_Association_Rule.py
+++ Main_Association_Rule.py
@@ -294,8 +294,10 @@ def main():
 
     best_confidence = 0.8    # Initialize the variables to change
     # Considering anomalies and nomals simultaneously
-
-    if file_type in ['CICModbus23', 'CICModbus']:
+    
+    if file_type in ['CICIDS2017', 'CICIDS', 'Kitsune']:
+        confidence_values = [0.05]
+    elif file_type in ['CICModbus23', 'CICModbus']:
         confidence_values = np.arange(0.1, 0.96, 0.05)
     # elif file_type in ['NSL-KDD', 'NSL_KDD']:
     #     confidence_values = np.arange(0.5, 0.96, 0.05)
